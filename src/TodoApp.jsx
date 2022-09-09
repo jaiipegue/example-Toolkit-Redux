4a28677b73348d4363d6726a45d7581144d2a130
Jaiipegue <jaiipegue12@gmail.com>
2022-09-09 00:02:19 -0500

    finish example thunsk

---
+++ src/TodoApp.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis";
+
+export const TodoApp = () => {
+  const [todoId, setTodoId] = useState(1);
+  const { data: todo, isLoading } = useGetTodoQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId(todoId + 1);
+  };
+
+  const prevTodo = () => {
+    if (todoId === 1) return;
+    setTodoId(todoId - 1);
+  };
+  return (
+    <>
+      <h1>Todo RTK Query</h1>
+      <hr />
+      <h4>isLoading: {isLoading ? "true" : "false"}</h4>
+      <pre>{JSON.stringify(todo)}</pre>
+
+      <button onClick={prevTodo}>previous Todo</button>
+      <button onClick={nextTodo}>next Todo</button>
+
+      {/* <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <strong> {todo.completed ? "DONE" : "Pending"} </strong>
+            {todo.title}
+          </li>
+        ))}
+      </ul> */}
+    </>
+  );
+};
